2c7d9539718e071d0246e6acb59b673998efec60
Anyon <zoujingli@qq.com>
2018-02-02 14:22:19 +0800

    [更新]更新微信第三方平台案例

--- Test/open.php
+++ Test/open.php
@@ -46,25 +46,19 @@ SQL;
 try {
 
     // 1. 手动加载入口文件
-    include "../include.php";
+    include "../vendor/autoload.php";
+    include "../WeOpen/WeChat.php";
 
-
+    // 2. 准备配置参数
     $config = [
-        'component_appid'          => 'test',
-        'component_token'          => 'test',
-        'component_appsecret'      => '71308e96a204296c57d7cd4b21b883e8',
-        'component_encodingaeskey' => 'BJIUzE0gqlWy0GxfPp4J1oPTBmOrNDIGPNav1YFH5Z5',
-        // 配置商户支付参数
-        'mch_id'                   => "1332187001",
-        'mch_key'                  => '11bd3d66d85f322a1e803cb587d18c3f',
-        // 配置商户支付双向证书目录
-        'ssl_key'                  => '',
-        'ssl_cer'                  => '',
-
+        'component_appid'          => 'wx4e63e993e222df8d',
+        'component_token'          => 'P8QHTIxpBEq88IrxatqhgpBm2OAQROkI',
+        'component_appsecret'      => '7cfa1afa87a41e2ea3445cea015c0974',
+        'component_encodingaeskey' => 'L5uFIa0U6KLalPyXckyqoVIJYLhsfrg8k9YzybZIHsx',
     ];
-    // 开放平台获取授权公众号 AccessToken 处理
+    // 注册授权公众号 AccessToken 处理
     $config['GetAccessTokenCallback'] = function ($authorizer_appid) use ($config) {
-        $open = new \WeChat\Open($config);
+        $open = new \WeOpen\WeChat($config);
         $authorizer_refresh_token = ''; // 从数据库去找吧，在授权绑定的时候获取到了
         $result = $open->refreshAccessToken($authorizer_appid, $authorizer_refresh_token);
         if (empty($result['authorizer_access_token'])) {
@@ -77,10 +71,14 @@ try {
         // 需要把$data记录到数据库
         return $result['authorizer_access_token'];
     };
-    // 使用第三方服务创建接口实例
-    $open = new \WeChat\Open($config);
+
+    // 3 使用第三方服务创建接口实例
+    $open = new \WeOpen\WeChat($config);
     $wechat = $open->instance('授权公众号APPID', 'User');
+
 } catch (Exception $e) {
+
     // 出错啦，处理下吧
     echo $e->getMessage() . PHP_EOL;
+
 }
